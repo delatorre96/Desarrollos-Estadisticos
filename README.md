24fc1ced9c6325372840297d78c6625cd99204c1
Nacho <idelatorre@mondragon.edu>
2024-07-01 15:58:55 +0200

    readme edition

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ This module provides utility functions for data manipulation and preprocessing.
 - **Functions:**
   - `columnasCategoricas(df)`: Identifies categorical columns in the dataframe.
   - `vars_interaccion(df)`: Creates interaction variables based on pairwise multiplication of columns.
-  - Add other data manipulation functions as needed.
 
 ### 2. `instrument_selection.py`
 
@@ -20,24 +19,32 @@ This module contains functions for selecting exogenous and endogenous instrument
 - **Functions:**
   - `instrumentos_exogenos(df, var_ind, var_dep, minUmbral, funcSeleccionVars)`: Identifies exogenous instruments.
   - `vars_endogena(df, var_ind, var_dep, minUmbral, funcSeleccionVars)`: Identifies endogenous variables and their instruments.
-  - Add other instrument selection functions as needed.
 
 ### 3. `regression_analysis.py`
 
 This module includes functions for regression analysis and handling multicollinearity.
 
 - **Functions:**
+  - `estimacion (varsIndependientes, varDependiente, df,intercept = False)` :Estimates the dependent variable using a regression model.
+  - `regresion(X, y, const=1)` : Perform OLS regression.
   - `residuo(varsIndependientes, varDependiente, df, intercept=False)`: Calculates residuals of a regression model.
   - `multicolinealidad(X)`: Detects multicollinearity in independent variables.
-  - Add other regression analysis functions as needed.
+  - `mc2e_tree(df, y, funcSeleccionVars = varsMasCorrelacionadas, minUmbral = 0.4)` : Construct a regression tree using instrumental variables. 
+  - `regTree (df, tree, y)` : Perform regression based on a regression tree.
+  - `regTreeSinMultiSinX (df, y, funcSeleccionVars, minUmbral, summary = True)` : Perform regression tree without multicollinearity and without using matrix X (this matrix is identified automatically).
+  - `regTreeMulti (df, tree, y)` : Perform regression with handling multicollinearity.
+  - `regTreeMultiSinX (df, y, minUmbral, funcSeleccionVars = varsMasCorrelacionadas, summary = True)`: Perform regression with handling multicollinearity without firstly using X (it detects best X)
 
 ### 4. `variable_fussion.py`
 
 This module deals with merging or fusing variables from different sources.
 
 - **Functions:**
-  - Include functions related to merging or fusing variables.
-  - Ensure functions are well-documented and modular for reusability.
+  - `enalzarVariables(dataframe, vars_enlazar, str_nueva_var, porc_varExplicada)`:This function performs variable linking using Principal Component Analysis (PCA).
+    It creates a new composite variable that captures a significant amount of explained variance
+    from the original set of variables.
+  - `busquedaEnlazamiento(dataframe, porcMinCorrelacion)`: This function searches for pairs of variables that have high correlation to be linked together.
+
 
 ### 5. `variable_selection.py`
 
@@ -46,7 +53,7 @@ This module contains functions for selecting important variables in regression m
 - **Functions:**
   - `varsMasCorrelacionadas(df, varDependiente, minCorr)`: Identifies variables most correlated with the dependent variable.
   - `varsMasImportantesArbol(df, varDependiente, minUmbral)`: Identifies important variables using decision tree models.
-  - Add other variable selection functions as needed.
+  - `reducir_variables_aleatoriamente(X, y, umbral_condicional, model_reg=True)` : Reduce variables randomly to manage multicollinearity.
 
 ## Usage
 
--- regression_analysis.py
+++ regression_analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 from variable_selection import varsMasCorrelacionadas, varsMasImportantesArbol,reducir_variables_aleatoriamente
+from instrument_selection import vars_endogena
 from sklearn.linear_model import LinearRegression
 
 
@@ -88,96 +89,7 @@ def multicolinealidad(X):
     matrix_correlacion_upper = matrix_correlacion.where(np.triu(np.ones(matrix_correlacion.shape), k=1).astype(np.bool))
     return any(matrix_correlacion_upper > 0.95)
 
-def instrumentos_exogenos (df, var_ind, var_dep, minUmbral, funcSeleccionVars = varsMasCorrelacionadas):
-    """
-    Identifies exogenous instruments for a set of independent variables.
-    
-    Parameters:
-    df (pandas.DataFrame): The input dataframe.
-    var_ind (list): List of independent variables.
-    var_dep (str): Dependent variable.
-    minUmbral (float): Minimum importance threshold.
-    funcSeleccionVars (function): Function to select variables.
-    
-    Returns:
-    dict: A dictionary indicating whether each variable is exogenous.
-    """
-    alfa = 0.05
-    instrumentos_exogenos = {}
-    for x_i in var_ind:
-        print('Evaluando',x_i,'...')
-        #Calculamos las vars más correlacionadas
-        minCorr = 1
-        while minCorr > 0:
-            if funcSeleccionVars == varsMasCorrelacionadas:
-                instrumentos_lista = [i for i in varsMasCorrelacionadas (varDependiente = x_i, df = df, minCorr = minUmbral) if i not in var_ind and i not in var_dep]
-            if funcSeleccionVars == varsMasImportantesArbol:
-                instrumentos_lista = [i for i in varsMasImportantesArbol (df = df, varDependiente = x_i, minUmbral = minUmbral) if i not in var_ind and i not in var_dep]
-            if len(instrumentos_lista) >= len(var_ind):
-                v = residuo (varsIndependientes = instrumentos_lista, varDependiente = x_i, df = df)
-                x_df = df.copy()
-                x_df = x_df[var_ind]
-                x_df['v'] = v
-                x_df = sm.add_constant(x_df)
-                model = sm.OLS(df[var_dep], x_df).fit()
-                p_values = dict(model.pvalues)            
-                if alfa < p_values['v']:
-                    instrumentos_exogenos.update({x_i : True})
-                    break
-                else:
-                    minCorr -= 0.01
-                    
-            else:
-                minCorr -= 0.01
-        instrumentos_exogenos.update({x_i : False})
-    return instrumentos_exogenos
-    
-def vars_endogena (df, var_ind, var_dep, minUmbral ,funcSeleccionVars = varsMasCorrelacionadas):
-    """
-    Identifies endogenous variables in a set of independent variables.
-    
-    Parameters:
-    df (pandas.DataFrame): The input dataframe.
-    var_ind (list): List of independent variables.
-    var_dep (str): Dependent variable.
-    minUmbral (float): Minimum importance threshold.
-    funcSeleccionVars (function): Function to select variables.
-    
-    Returns:
-    dict: A dictionary of endogenous variables and their instruments.
-    """
-    alfa = 0.05
-    instrumentos = {}
-    for x_i in var_ind:
-        #Calculamos las vars más correlacionadas
-        minCorr = 1
-        while minCorr > 0:
-            if funcSeleccionVars == varsMasCorrelacionadas:
-                instrumentos_lista = [i for i in varsMasCorrelacionadas (varDependiente = x_i, df = df, minCorr = minUmbral) if i not in var_ind and i not in var_dep]
-            if funcSeleccionVars == varsMasImportantesArbol:
-                instrumentos_lista = [i for i in varsMasImportantesArbol (varDependiente = x_i, df = df, minUmbral = minUmbral) if i not in var_ind and i not in var_dep]               
-            if len(instrumentos_lista) >= len(var_ind):
-                break
-            else:
-                minCorr -= 0.01     
-        v = residuo (varsIndependientes = instrumentos_lista, varDependiente = x_i, df = df)
-        x_df = df.copy()
-        x_df = x_df[var_ind]
-        x_df['v'] = v
-        x_df = sm.add_constant(x_df)
-        model = sm.OLS(df[var_dep], x_df).fit()
-        p_values = dict(model.pvalues)
-        if alfa > p_values['v']:
-            instrumentos.update({x_i : instrumentos_lista})
-    #Evaluamos los instrumentos sean exógenos dentro de la misma regresion VI
-    #for i in instrumentos:
-    #    if instrumentos[i] == None:
-    #        pass
-     #   else:
-     #       instrumentos_exogenos (df = df, var_ind = instrumentos[i], var_dep = i)
-    
-            print('Encontramos que',x_i,'es endógena')
-    return instrumentos
+
 
 def mc2e_tree(df, y, funcSeleccionVars = varsMasCorrelacionadas, minUmbral = 0.4):
     """
